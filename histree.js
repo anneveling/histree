9a4cf767b209c0c3a3156ee92df00a5530c4252f
Boaz Leskes <b.leskes@gmail.com>
2012-11-23 15:49:19 +0100

    Added a mocked view local storage content

--- histree.js
+++ histree.js
@@ -1,15 +1,31 @@
 
 function mockGetAll(callback) {
-  callback([]
-  )
+  callback([HistoryNode("http://www.google.com/?q=yes","YES"),HistoryNode("http://www.google.com/?q=we","WE"),
+            HistoryNode("http://www.google.com/?q=can","CAN")
+           ]
+  );
 }
 
 
+function showStorageContent() {
+  var content = $('#localStorageView .content');
+
+  content.html();
+  var ul = $(document.createElement("ul"));
+  content.append(ul);
+  mockGetAll(function(nodes) {
+    $.each(nodes,function (i,node){
+      var li = $(document.createElement("li"));
+      li.text(JSON.stringify(node));
+      ul.append(li);
+    });
+  });
+}
+
 function init() {
   var histRoot = $('#history');
-  getall(function (items) {
-    histRoot.html(items.length);
-  });
+
+  $('#localStorageView button').click(showStorageContent);
 }
 
 $(document).ready(init);
