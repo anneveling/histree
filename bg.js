0eb45dc192f6775f56223c516dffa4653f8163d7
Anne Veling <anne@beyondtrees.com>
2012-11-23 18:14:32 +0100

    moved tabstate to in memory structure

--- bg.js
+++ bg.js
@@ -7,31 +7,28 @@ function log(s) {
 
 // EVENTS 
 
+//tabstate
+var tabStates = {};
+
+function findTabState(windowId, tabId) {
+	var tsid = createTabStateId(windowId, tabId);
+	return tabStates[tsid];
+}
+function putTabState(ts) {
+	tabStates[ts.id] = ts;
+}
+function createTabStateId(windowId, tabId) {
+	return windowId + "-" + tabId;
+}
+
+// UTILITIES
+
 function now() {
 	return new Date().getTime();
 }
 
 function generateId() {
 	return "h" + now();
-}
-
-function createTabStateId(windowId, tabId) {
-	return windowId + "-" + tabId;
-}
-
-function findTabState(windowId, tabId, callback) {
-	var ts = {};
-	ts.id = createTabStateId(windowId, tabId);
-	ts.windowId = windowId;
-	ts.tabId = tabId;
-
-	getOrCreate(ts.id, ts, callback);
-}
-
-function findTabStateMaybe(windowId, tabId, callbackFound, callbackNotFound) {
-	var tsid = createTabStateId(windowId, tabId);
-
-	getMaybe(tsid, callbackFound, callbackNotFound);
 }
 
 
@@ -47,7 +44,20 @@ function setParent(node, parentId) {
 }
 
 function handleUpdate(tab) {
-	//
+
+	//see if another event has already handled this event
+	var ts = findTabState(tab.windowId, tab.id);
+	if (ts && ts.url == tab.url) {
+		console.log("sorry, but this event was already handled. ignoring");
+		return;
+	}
+	//mark we're handling this
+	ts = {};
+	ts.id = createTabStateId(tab.windowId, tab.id);
+	ts.url = tab.url;
+
+	putTabState(ts);
+
 	var node = {};
 	node.id = generateId();
 	node.timestamp = now();
@@ -63,36 +73,35 @@ function handleUpdate(tab) {
 	save(node, function() {
 		//find the parent node if we have one
 
+		//mark this tab now has this child
+		ts.currentId = node.id;
+		putTabState(ts);
+
 		//maybe this tab already had something else open
 		//we are going to assume that is this node's parent then
-		findTabState(tab.windowId, tab.id, function(prev) {
+		var prev = findTabState(tab.windowId, tab.id);
+		if (prev) {
 			console.log("found previous tabstate for this tab: " + prev);
 			if (prev.currentId) {
 				setParent(node, prev.currentId);
 			}
-		});
+		}
 
 		//only if node does not have a parent yet TODO
 
 		//does this tab have an opener-tab-id?
 		if (tab.openerTabId) {
 			//assume same window?
-			findTabState(tab.windowId, tab.openerTabId, function(opener) {
+			var opener = findTabState(tab.windowId, tab.id);
+			if (opener) {
 				console.log("found opener tabstate: " + opener);
 				if (opener.currentId) {
 					setParent(node, opener.currentId);
 				}
-			});
+			}
 		}		
 	});
 
-	//update the TabState for this window and tab
-	//TODO only after the other is read
-	findTabState(tab.windowId, tab.id, function(thisState) {
-		thisState.currentId = node.id;
-		thisState.url = tab.url;
-		save(thisState);
-	});	
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
@@ -109,32 +118,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if (changeInfo.status == "complete") {
 		console.log("tab.onUpdated COMPLETE");
 
-		//we actually get the oncomplete event multiple times
-		//did we already handle this event?
-		findTabStateMaybe(tab.windowId, tab.id, function(ts) {
-			//if it is found, check if the current has that url
-			if (ts.url != tab.url) {
-				//something new
-				handleUpdate(tab);
-	
-			} else {
-				console.log("duplicate onComplete event found, ignoring");
-			}
-		}, function() {
-			//no such tab state found
-			//new
-			var ts = {};
-			ts.id = createTabStateId(tab.windowId, tab.id);
-			ts.url = tab.url;
-			ts.window = tab.windowId;
-			ts.tabId = tab.id;
-			save(ts, function() {
-				handleUpdate(tab);
-			});
-		});
-
-		
-
+		handleUpdate(tab);
 	}
 		
 	
