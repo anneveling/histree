98adf5b223ae4ee4c4fdaf973e3a658a5c9dc662
Anne Veling <anne@beyondtrees.com>
2012-11-23 22:08:35 +0100

    setup new html

--- histree.js
+++ histree.js
@@ -1,18 +1,4 @@
-
-function mockGetAll(callback) {
-  callback([HistoryNode("http://www.google.com/?q=yes","YES"),HistoryNode("http://www.google.com/?q=we","WE"),
-            HistoryNode("http://www.google.com/?q=can","CAN")
-           ]
-  );
-}
-
-
 function showStorageContent() {
-  var content = $('#localStorageView .content');
-
-  content.html('');
-  var ul = $(document.createElement("ul"));
-  content.append(ul);
   getall(function(nodes) {
     $.each(nodes,function (i,node){
       console.log(node);
@@ -25,49 +11,24 @@ function clearStorage() {
   showStorageContent();
 }
 
-function node2html(node,parent) {
-  var li = $(document.createElement("li")),
-      a =  $(document.createElement("a"));
-
-  parent.append(li);
-
-  a.attr("href",node.url);
-  a.text(node.title);
-  li.append(a);
-  li.append($(' <span class="visited"> ('+ (new Date(node.timestamp)).toTimeString() +')</span> &nbsp; '));
-
-  a = $(document.createElement("a"));
-  li.append(a);
-  a.text("+");
-  a.attr("href","#");
-  a.click(function (event) {
-      buildSubTree(li,function (n) { return n.parentId == node.id; });
-      return false;
-  });
-
-
-
-}
-
-function buildSubTree(parent,filter) {
-  var ul = $(document.createElement("ul"));
-  parent.append(ul);
-  getAllNodes(function (nodes) {
-            $.each(nodes, function (i,node) {
-              node2html(node,ul);
-            })
-           },
-        filter
-  );
-}
-
 function buildHistoryTree() {
-  buildSubTree($('#history'),function (node) { return !node.parentId;  });
+  $('#history').html('');
+
+  getAllRootNodes(function(node) {
+    console.log("adding div for root node: " + node.id);
+    var dimensions = getSizeForTree(node);
+    var c = $(document.createElement("div")).addClass("treecontainer");
+    c.attr("id", node.id);
+    c.width(dimensions.width);
+    c.height(dimensions.height);
+
+    $('#history').append(c);
+
+  });  
 }
+
 
 function init() {
-
-
   buildHistoryTree();
 
   $('#showStorage').click(showStorageContent);
