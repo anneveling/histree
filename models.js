31ca7662a0d9b16831e97be5c65a945ea3160114
Boaz Leskes <b.leskes@gmail.com>
2012-11-23 16:00:05 +0100

    fixed HistoryNode bug: timestamp was called now.

--- models.js
+++ models.js
@@ -2,5 +2,5 @@
 
 function HistoryNode(url,title) {
   var n = now();
-  return { id: "h" + n, now: n, "url":url, "title": title, childrenIds: [] };
+  return { id: "h" + n, timestamp: n, "url":url, "title": title, childrenIds: [] };
 }
