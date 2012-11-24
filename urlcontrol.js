033c1d5cb0079c51e6154b78e80b8c71f8664d31
Boaz Leskes <b.leskes@gmail.com>
2012-11-24 13:45:39 +0100

    white listed http urls . The rest we ignore

--- urlcontrol.js
+++ urlcontrol.js
@@ -1,12 +1,11 @@
 
-
 URL_BLACK_LIST = [
-    new RegExp("^chrome://"),
   new RegExp("^http.?://www\\.google\\.[^/]+/url")
 ]
 
 function isUrlRelevantForHistree(url) {
   if (!url) return false;
+  if (url.substring(0,4) != "http") return false; // we only care about http
   for (var i=0;i<URL_BLACK_LIST.length;i++) {
     if (URL_BLACK_LIST[i].test(url)) return false;
   }
